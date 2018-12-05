f2e0674b334964a363e58f5ee3fee0a80c2633c1
Tech-A03x <tech-a03x@Tech-A03s-MacBook-Air.local>
2018-12-05 10:16:54 +0000

    undefined operator fixed

--- main.js
+++ main.js
@@ -1,96 +1,103 @@
-// // need a countdown 
-
-// append to the top div 
+// ***********************************psuedo code*****************************************
+// produce random 2 digit math problem
+// append that to the top div
 // capture the answer of the random problem
 // event listener on the button
 //  compare the value of the input with the captured answer
-// with the captured answer from 
-// line 3
 // if they are euqal, append
-// correct to answer div
+// correct to result div
 // else, append incorrect
 
-//clear the input 
-//make, append and capture a new random problem
-//random problem
+// clear the input 
+// make, append and capture a new random problem
+// *************************************psuedo code **************************************
 
-// creating a variable for the correct answe to hold 
+// creating a variable for the correct answer to hold 
 var correctAnswer;
 
-// funtion to reset the input value 
+// funtion to clear the input value 
 function resetInput() {
   document.getElementById("answer").value = '';
 }
-resetInput();
 
-// Creating random numbers for quiz ****************************************************
+
+// Creating random numbers for quiz 
 // /*used Math.random to create a random number from 0 < 1, multimplied it then by 10 to get numbers between 0 < 10 and then added 1 to get numbers from 1 <= 10. Used Math.floor to round down any decimal number*/
 function randomNumber() {
   return Math.floor(Math.random() * 10 + 1);
 }
+
+// function to randomise operator
+function randomOperator(operator) {
+  if (operator == 0) {
+    return "+";
+  } else if (operator == 1) {
+    return "-";
+  } else if (operator == 2) {
+    return "/";
+  }
+  else if (operator == 3) {
+    return "x";
+  }
+}
+
 // created a function to randomise qustions. 
 function makeQuestion() {
   var num1 = randomNumber();
   var num2 = randomNumber();
-  // statement 
-  var operatorString = randomOperator();
+  var operatorString = randomOperator(Math.floor(Math.random() * 3))
+
+
+  // creating a statement to randomise operator
   if (operatorString === "+") {
     correctAnswer = num1 + num2;
   } else if (operatorString === "-") {
     correctAnswer = num1 - num2;
-  } else if (operatorString === "*") {
+  } else if (operatorString === "x") {
     correctAnswer = num1 * num2;
-  } else if (operatorString === "/") {
+  } else if (operatorString == "/") {
     correctAnswer = num1 / num2;
   }
-
-  document.getElementById('question').innerText = "";
+  // Every single time you answer the question, new question should refresh
+  document.getElementById('question').innerHTML = " ";
+  // question should appear on question box with randomised numbers and operator
   document.getElementById('question').append(`${num1} ${operatorString} ${num2}`);
 }
-//Function to check correct answer and response 
+
+//Function to check correct answer and respond
 function checkAnswer() {
-  var answer = document.getElementById('answer').value
-  console.log(answer);
+  // checks the iput against the correct answer
+  var answer = document.getElementById('answer').value;
   if (answer == correctAnswer) {
-    return 'Well done! your answer is right.';
+    return 'Well done! your answer is right. Keep Moving!';
   } else {
     return 'Damn! your answer is wrong.';
   }
 }
-// function to randomise operator
-function randomOperator() {
-  if (Math.random() >= 0 && Math.random() <= 0.25) {
-    return "+";
-  } else if (Math.random() > 0.25 && Math.random() <= 0.5) {
-    return "-";
-  } else if (Math.random() > 0.5 && Math.random() <= 0.75) {
-    return "/";
-  }
-  else if (Math.random() > 0.75 && Math.random() < 1) {
-    return "*";
-  }
-}
-
 
 function appendResult(result) {
-  document.getElementById('result').innerText = '';
+  // refreshes response 
+  document.getElementById('result').innerHTML = '';
+  // prints out the response 
   document.getElementById('result').append(result);
 }
 
-document.getElementById('checkAnswer').addEventListener('click', function resetForClick() {
+// after clicking the button, the question box resets with a new question, empty input as well as giving a response for the current answer. 
+document.getElementById('checkAnswer').addEventListener('click', function resetWhenClicked() {
+  // This will first call check answer which will return either correct or incorrect, and then append result will be invoked with either correct or incorrect.
   appendResult(checkAnswer());
   resetInput();
   makeQuestion();
 })
 
+// When calling this function, you produce the randomised numbers,operator, and set the correct answer as well as append it to the question div
 makeQuestion();
 
+// need to add points as global variable;
 
 
-// ********************************************************** countdown stack overflow ********************************************
-var count = 60, timer = setInterval(function () {
-  $("#counter").html(count--);
-  if (count == 1) clearInterval(timer);
-}, 1000);
+// ********************************************************** countdown timer ********************************************************
+
+
 
 
